236a3527f0cb8ef8d7c3c64e17ef84400b173e8a
Selma Duric <selma@duric.ca>
2021-03-03 15:01:29 -0800

    Add test directories.

---
+++ R/instaeda.R
@@ -0,0 +1,41 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_intro <- function(){
+  NULL
+}
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_corr <- function(){
+  NULL
+}
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+divide_and_fill <- function(){
+  NULL
+}
+
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_basic_distributions <- function(){
+  NULL
+}
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(instaeda)
+
+test_check("instaeda")
