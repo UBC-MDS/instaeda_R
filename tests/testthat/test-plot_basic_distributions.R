f1dd7e5e99a9460e6e7b2d119b097bed5f1c954e
Justin Fu <jufu11@gmail.com>
2021-03-11 22:42:36 -0800

    updated to ggthemes for plot themes

--- tests/testthat/test-plot_basic_distributions.R
+++ tests/testthat/test-plot_basic_distributions.R
@@ -36,51 +36,16 @@ test_that("All plots using GeomBar", {
   expect_equal(sapply(basic_distb_plots[['year']]$layers, function(x) class(x$geom)[1]), 'GeomBar')
 })
 
+test_that("Invalid function parameter tests", {
+  expect_error(plot_basic_distributions(NULL),"Data provided is not a data frame")
+  expect_error(plot_basic_distributions(input_df,include="not a valid input"),"include parameter invalid. Please choose NULL, 'number' or 'string'")
+  expect_warning(plot_basic_distributions(input_df,ggtheme="IncorrectColor"),"Only the following ggthemes are supported: theme_gdocs, theme_excel,
+            theme_economist, theme_wsj, theme_solarized")
 
-# test_that("resulting mark is geom_tile", {
-#   expect_equal(class(plot_corr(input_df)$layers[[1]]$geom)[1], "GeomTile")
-# })
-#
-# test_that("map 'variable_1' to x-axis,
-#           'variable_2' to y-axis,
-#           'corr' to colour", {
-#             expect_true(rlang::get_expr(
-#               plot_corr(input_df)$mapping$x
-#             ) == as.symbol("variable_1"))
-#             expect_true(rlang::get_expr(
-#               plot_corr(input_df)$mapping$y
-#             ) == as.symbol("variable_2"))
-#             expect_true(rlang::get_expr(
-#               plot_corr(input_df)$mapping$fill
-#             ) == as.symbol("corr"))
-#           })
-#
-# test_that("range of correlation values between -1, 1", {
-#   expect_equal(plot_corr(input_df)$scales$scales[[1]]$limits[1], -1)
-#   expect_equal(plot_corr(input_df)$scales$scales[[1]]$limits[2], 1)
-# })
-#
-# test_that("Input is a dataframe", {
-#   expect_error(plot_corr(c("a", "b")), "Data provided is not a data frame")
-# })
-#
-# test_that("Input must contain >= 2 numeric columns", {
-#   expect_error(
-#     plot_corr(data.frame(a = c("a", "b", "c"), b = c("d", "e", "f"))),
-#     "Need at least two numeric columns to calculate correlation"
-#   )
-# })
-#
-# test_that("Correlation method must be appropriate", {
-#   expect_error(
-#     plot_corr(input_df, method = "kendll"),
-#     "Correlation method not acceptable"
-#   )
-# })
-#
-# test_that("Colour palette not diverging", {
-#   expect_warning(
-#     plot_corr(input_df, colour_palette = "Blues"),
-#     "Recommended ggplot continuous diverging colour palette"
-#   )
-# })
+})
+
+test_that("Select specific columns from data frame", {
+  expect_equal(length(plot_basic_distributions(input_df,cols=c('sex','year'))), 2)
+})
+
+
