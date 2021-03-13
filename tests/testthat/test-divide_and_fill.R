2c5c89187f6079cfa4e238365698715a0db5e9d8
Zeliha Ural Merpez <zelhaaa@gmail.com>
2021-03-13 14:59:41 -0800

    Added function and tests

---
+++ tests/testthat/test-divide_and_fill.R
@@ -0,0 +1,65 @@
+library(testthat)
+library(instaeda)
+library(palmerpenguins)
+
+input_df <- palmerpenguins::penguins
+
+test_that("test input as a dataframe", {
+  expect_error(divide_and_fill(c("a", "b")), "Dataframe must be a dataframe.")
+})
+
+test_that("test input as a dataframe", {
+  expect_error(divide_and_fill(1), "Dataframe must be a dataframe.")
+})
+
+test_that("test verbose", {
+  expect_error(divide_and_fill(input_df, verbose="a"), "Verbose must be an integer.")
+})
+
+test_that("test cols", {
+  expect_error(divide_and_fill(input_df, cols=5), "The input cols must be of type character belong to the column
+            names for input dataframe!")
+})
+
+test_that("test cols", {
+  expect_error(divide_and_fill(input_df, cols=NA), "The input cols must be of type character belong to the column
+            names for input dataframe!")
+})
+
+test_that("test cols", {
+  expect_error(divide_and_fill(input_df, cols=c("species")), "All items in list cols must be numeric.")
+})
+
+test_that("test strategy", {
+  expect_error(divide_and_fill(input_df, strategy = 'madeup'), "Can only use these strategies: mean, median, majority, or random")
+})
+
+test_that("test random", {
+  expect_error(divide_and_fill(input_df, random = 'madeup'), "Random must be logical.")
+})
+
+
+test_that("test return object dataframe", {
+  filled_df <- divide_and_fill(input_df)
+  expect_true(is.data.frame(filled_df))
+})
+
+test_that("test return object dataframe", {
+  filled_df <- divide_and_fill(input_df, cols = c("bill_length_mm", "body_mass_g"))
+  expect_true(is.data.frame(filled_df))
+})
+
+test_that("test return object dataframe", {
+  filled_df <- divide_and_fill(input_df, cols = "bill_length_mm")
+  expect_true(is.data.frame(filled_df))
+})
+
+test_that("test return object dataframe", {
+  filled_df <- divide_and_fill(input_df, cols = "bill_length_mm", strategy = "median", verbose = 0L)
+  expect_true(is.data.frame(filled_df))
+})
+
+test_that("test return object dataframe", {
+  filled_df <- divide_and_fill(input_df, cols = "bill_length_mm", verbose = 0L)
+  expect_true(is.data.frame(filled_df))
+})
